d572af70769303680bc1c152153a29bad2120232
nabil <nabildotkhalil@gmail.com>
2017-11-22 20:42:13 +0100

    added marker and restyled the map

--- src/map/map.component.ts
+++ src/map/map.component.ts
@@ -8,8 +8,9 @@ import { Component } from '@angular/core';
   `],
   template: `
     <div>
-     <!--img src="../assets/map_schiphol.png"/-->
-     <agm-map [latitude]="lat" [longitude]="lng" [zoom]="18"></agm-map>
+     <agm-map [latitude]="lat" [longitude]="lng" [zoom]="18" [styles]="styles">
+       <agm-marker [latitude]="lat" [longitude]="lng"></agm-marker>
+     </agm-map>
      </div>
     <a class="btn-primary" ngbButtonLabel routerLink="/timeline" routerLinkActive="active">Go back to timeline</a>
   `
@@ -17,5 +18,8 @@ import { Component } from '@angular/core';
 export class MapComponent {
   lat: number = 52.30962393544346;
   lng: number = 4.763941168785095;
+
+  styles =[{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}];
+
 }
 
